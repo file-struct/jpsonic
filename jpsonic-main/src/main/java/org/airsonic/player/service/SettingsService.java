airsonic/airsonic

In HSQLDB 2, 'all VARCHAR column type declaration require a size' by default. To change the behavior, append 'sql.enforce_size=false' to the default JDBC URL. http://hsqldb.org/doc/guide/dbproperties-chapt.html#N1580D
Signed-off-by: Iwao AVE! <harawata@gmail.com>

airsonic-main/src/main/java/org/airsonic/player/service/SettingsService.java
@@ -286,7 +286,7 @@ public class SettingsService {
     }
 
     public static String getDefaultJDBCUrl() {
-        return "jdbc:hsqldb:file:" + getAirsonicHome().getPath() + "/db/" + getFileSystemAppName();
+        return "jdbc:hsqldb:file:" + getAirsonicHome().getPath() + "/db/" + getFileSystemAppName() + ";sql.enforce_size=false";
     }
 
     public static File getLogFile() {

file-struct/jpsonic

jpsonic-main/src/main/java/org/airsonic/player/service/SettingsService.java
@@ -363,7 +363,7 @@ public class SettingsService {
     }
 
     public static String getDefaultJDBCUrl() {
-        return "jdbc:hsqldb:file:" + getJpsonicHome().getPath() + "/db/" + getFileSystemAppName();
+        return "jdbc:hsqldb:file:" + getJpsonicHome().getPath() + "/db/" + getFileSystemAppName() + ";sql.enforce_size=false";
     }
 
     public static int getDefaultUPnPPort() {